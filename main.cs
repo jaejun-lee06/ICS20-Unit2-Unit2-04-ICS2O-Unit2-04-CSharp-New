9e356510ab3fb7fb10f1ed218fed1f094928c8b5
jaejun-lee06 <99355321+jaejun-lee06@users.noreply.github.com>
2022-04-04 22:05:23 +0000

    fix

--- main.cs
+++ main.cs
@@ -2,9 +2,7 @@
 // Created on: Mar 2022
 //
 // This program accepts user input
-
 using System;
-
 class Program
 {
     public static void Main(string[] args)
@@ -13,17 +11,14 @@ class Program
         int length;
         int width;
         int area;
-
         Console.WriteLine("This program calculates the area of a triangle.");
         Console.WriteLine("");
         Console.Write("Enter the base (cm): ");
         length = Convert.ToInt32(Console.ReadLine());
         Console.Write("Enter the height (cm): ");
         width = Convert.ToInt32(Console.ReadLine());
-
        //process
         area = (length * width) / 2;
-
        //output
         Console.WriteLine("");
         Console.Write("The area of the triangle = " + area + " cm²");
