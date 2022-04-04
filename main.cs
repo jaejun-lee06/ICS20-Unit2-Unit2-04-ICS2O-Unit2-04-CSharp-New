8e68b27a0057043dfa30a4c3eb6c7ce16ea19bce
jaejun-lee06 <99355321+jaejun-lee06@users.noreply.github.com>
2022-04-04 21:46:52 +0000

    fix

--- main.cs
+++ main.cs
@@ -9,21 +9,24 @@ class Program
 {
     public static void Main(string[] args)
     {
-        // This function calculates the area and perimeter of a triangle
-        int Base;
-        int Height;
+        // input
+        int length;
+        int width;
+        int area;
 
         Console.WriteLine("This program calculates the area of a triangle.");
         Console.WriteLine("");
-        Console.Write("Enter the base (mm): ");
-        Base = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Enter the height (mm): ");
-        Height = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the base (cm): ");
+        length = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the height (cm): ");
+        width = Convert.ToInt32(Console.ReadLine());
 
-        Double Area = (Base * Height) / 2;
+      //process
+        area = (length * width) / 2;
 
+      //output
         Console.WriteLine("");
-        Console.Write("Area of the triangle = " + Area + " mm²");
+        Console.Write("The area of the triangle = " + area + " cm²");
         Console.ReadKey();
         Console.WriteLine("\nDone.");
     }
